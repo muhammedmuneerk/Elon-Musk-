5b9d415a09452333786dde19a498128241b64651
muhammedmuneerk <muhammedmuneerk15@gmail.com>
2024-02-20 18:57:40 +0530

    elon musk website added gmail recieviing added

---
+++ script.js
@@ -0,0 +1,93 @@
+const form = document.querySelector("form");
+const fullName = document.getElementById("name");
+const email = document.getElementById("email");
+const phone = document.getElementById("phone");
+const subject = document.getElementById("subject");
+const mess = document.getElementById("message");
+
+function sendEmail() {
+    const bodyMessage = `Full Name: ${fullName.value}<br> Email: ${email.value}<br> Phone Number: ${phone.value}<br> Message: ${mess.value}`;
+
+    Email.send({
+        SecureToken : "33fd254f-3ad3-41f0-9307-94c41e564d79",
+        To : 'muhammedmuneerk15@gmail.com',
+        From : "muhammedmuneerk15@gmail.com",
+        Subject : subject.value,
+        Body : bodyMessage
+    }).then(
+        message => {
+            if (message == "OK") {
+                Swal.fire({
+                    title: "Success!",
+                    text: "Message sent successfully!",
+                    icon: "success"
+                  });
+            }
+        }
+    );
+}
+
+function checkInputs() {
+    const items = document.querySelectorAll(".item");
+
+    for (const item of items) {
+        if (item.value == "") {
+            item.classList.add("error");
+            item.parentElement.classList.add("error");
+        }
+
+        if (items[1].value != "") {
+            checkEmail();
+        }
+
+        items[1].addEventListener("keyup", () => {
+            checkEmail();
+        })
+
+        item.addEventListener("keyup", () => { 
+            if (item.value != "") {
+                item.classList.remove("error");
+                item.parentElement.classList.remove("error");
+            }
+            else {
+                item.classList.add("error");
+                item.parentElement.classList.add("error");
+            }
+        });
+    }
+}
+
+function checkEmail() {
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    const errorTextEmail = document.querySelector(".error-text.email")
+
+    if (!email.value.match(emailRegex)) {
+        email.classList.add("error");
+        email.parentElement.classList.add("error");
+        
+        if (email.value != "") {
+            errorTextEmail.innerText = "Enter a valid email address";
+        }
+        else {
+            errorTextEmail.innerText = "Email Address Can't be blank";
+        }
+    }
+    else {
+        email.classList.remove("error");
+        email.parentElement.classList.remove("error");
+    }
+}
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    checkInputs();
+
+    if (!fullName.classList.contains("error") && !email.classList.contains("error") && 
+    !phone.classList.contains("error") && !subject.classList.contains("error") && 
+    !mess.classList.contains("error")) {
+        sendEmail();
+
+        form.reset();
+        return false;
+    }
+});
